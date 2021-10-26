4ec0730c60ca9934c2be5efb9f454f6106ca3949
Mrunmai Abhyankar <kshitiabhyankar@gmail.com>
2021-10-26 17:09:01 +0530

    Palette added

---
+++ aadhaar_address_update/lib/config/theme.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Palette {
+  static const Color black = Colors.black;
+  static const Color white = Colors.white;
+  static const Color primary = Color(0xFF7B1FA2);
+  static const Color text = Color(0xFF20082B);
+  static const Color background = Color(0xFFFBF6FD);
+  static const Color shade1 = Color(0xFF5A1777);
+  static const Color shade2 = Color(0xFF9B27CD);
+  static const Color shade3 = Color(0xFFB14ADC);
+  static const Color shade4 = Color(0xFFC375E5);
+}
